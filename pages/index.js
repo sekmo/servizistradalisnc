9ab769a1b79764bffdee31178375c057dc1d9c17
sekmo <francescojjmari@gmail.com>
2022-09-25 00:45:26 +0200

    Home page slider

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import Flickity from 'react-flickity-component'
+import 'flickity/dist/flickity.min.css';
+
+const flickityOptions = {
+    initialIndex: 1,
+    prevNextButtons: false,
+    pageDots: false,
+    wrapAround: true,
+    autoPlay: 2000
+}
 
 const user = {
   name: 'Tom Cook',
@@ -27,7 +37,7 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <div className="min-h-screen bg-slate-800 text-indigo-500">
+    <div className="min-h-screen bg-white text-indigo-500">
       <Disclosure as="nav" className="bg-emerald-800 border-b border-lime-300">
         {({ open }) => (
           <>
@@ -97,31 +107,18 @@ export default function Example() {
         )}
       </Disclosure>
 
-      <div className="relative bg-gray-800 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
-        <div className="absolute inset-0 overflow-hidden">
-          <img
-            src="https://tailwindui.com/img/ecommerce-images/home-page-03-feature-section-full-width.jpg"
-            alt=""
-            className="w-full h-full object-center object-cover"
-          />
-        </div>
-        <div aria-hidden="true" className="absolute inset-0 bg-gray-900 bg-opacity-50" />
-        <div className="relative max-w-3xl mx-auto flex flex-col items-center text-center">
-          <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">I nostri servizi</h2>
-          <ul className="mt-3 text-xl text-white">
-            <li>Movimento Terra</li>
-            <li>Sistemazione verde pubblico</li>
-            <li>Lavori Edili e Stradali c/o terzi</li>
-          </ul>
-
-          <a
-            href="#"
-            className="mt-8 w-full block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto"
-          >
-            Read our story
-          </a>
-        </div>
-      </div>
+      <Flickity
+        className={'carousel'} // default ''
+        elementType={'div'} // default 'div'
+        options={flickityOptions}
+        disableImagesLoaded={false} // default false
+        reloadOnUpdate // default false
+        static // default false
+      >
+        <div class="slide bg-black"><img src="/slider-home/1.jpg" class="object-cover min-w-full min-h-full h-full" /></div>
+        <div class="slide bg-black"><img src="/slider-home/2.jpg" class="object-cover min-w-full min-h-full h-full" /></div>
+        <div class="slide bg-black"><img src="/slider-home/3.jpg" class="object-cover min-w-full min-h-full h-full" /></div>
+      </Flickity>
 
       <div className="py-10">
         <header>
