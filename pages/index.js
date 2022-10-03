e23813c70482e99f6f964eade1f912c6e1ba0282
sekmo <francescojjmari@gmail.com>
2022-10-03 16:51:44 +0200

    Extract navigation component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import Navigation from '../components/Navigation'
 import Flickity from 'react-flickity-component'
 import 'flickity/dist/flickity.min.css';
 
@@ -19,7 +16,7 @@ const user = {
   imageUrl:
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
-const navigation = [
+const navigation_items = [
   { name: 'Home', href: '#', current: true },
   { name: 'Azienda', href: '#azienda', current: false },
   { name: 'Servizi', href: '#servizi', current: false },
@@ -64,82 +61,10 @@ const realizzazioni = [
   { name: "Realizzazione di gabbionate", src: "realizzazioni/10.jpg", description: "" },
 ]
 
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
 export default function Example() {
   return (
     <div className="min-h-screen bg-white">
-      <Disclosure as="nav" className="bg-emerald-800 border-b border-lime-300">
-        {({ open }) => (
-          <>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="flex justify-between h-16">
-                <div className="flex">
-                  <div className="flex-shrink-0 flex items-center">
-                    {/* <Image src="/logo_con_scritta.svg" alt="Servizi Stradali SNC" width={72} height={16} /> */}
-                    <img
-                      className="block h-8 sm:h-9 w-auto mb-3"
-                      src="/logo_con_scritta.svg"
-                      alt="Servizi Stradali SNC"
-                    />
-                  </div>
-                  <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'border-lime-300 text-white'
-                            : 'border-transparent text-gray-300 hover:border-gray-300 hover:text-gray-400',
-                          'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-                <div className="-mr-2 flex items-center sm:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-              </div>
-            </div>
-
-            <Disclosure.Panel className="sm:hidden">
-              <div className="pt-2 pb-3 space-y-1 bg-white">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className={classNames(
-                      item.current
-                        ? 'bg-indigo-50 border-indigo-500 text-indigo-700'
-                        : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
-                      'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
-                    )}
-                    aria-current={item.current ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
+      <Navigation navigation_items={navigation_items}></Navigation>
 
       <Flickity
         className={'carousel'} // default ''
