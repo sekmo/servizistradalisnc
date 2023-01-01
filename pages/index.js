4107e798f459325e032e04c7eb75431e6a499d62
sekmo <francescojjmari@gmail.com>
2023-01-02 00:40:42 +0100

    Switch contact with certifications

--- pages/index.js
+++ pages/index.js
@@ -169,19 +169,7 @@ export default function Example() {
             <div className="max-w-xl sm:max-w-7xl mx-auto pb-8 px-4 sm:px-6 lg:pb-10 lg:px-8">
               <section id="contatti">
                 <div className="md:grid md:grid-cols-2 lg:gap-20 pt-8 sm:pt-16">
-                  <div id="certifications" className="sm:lg-0 mx-auto sm:m-0 text-center sm:text-left">
-                    <span className="block text-sm text-gray-400">Azienda certificata</span>
-                    <img src="soa5.png" alt="Certificazione SOA" className="block w-40 sm:w-52 mt-1 block mx-auto sm:m-0 sm:mt-1" />
-                    <p className="mt-4 text-gray-400 mx-auto sm:ml-0 max-w-xs sm:max-w-sm text-sm">Abbiamo conseguito la certificazione SOA categoria OG3 (classifica II) e categoria OG1 (classifica II).</p>
-                    {
-                      /* <ul className="inline-block list-disc mt-4 text-sm px-4">
-                        <li>Certificazione SOA cat. OG3 classifica II</li>
-                        <li>Certificazione SOA cat. OG1 classifica II</li>
-                      </ul> */
-                    }
-                  </div>
-
-                  <div id="contact" class="mt-16 md:-mt-2">
+                  <div id="contact" className="sm:lg-0 mx-auto sm:m-0 text-center sm:text-left">
                     <h1 className="block mt-2 text-3xl text-center leading-8 font-extrabold tracking-tight sm:text-4xl mb-4 sm:text-left">
                       Contatti
                     </h1>
@@ -218,6 +206,18 @@ export default function Example() {
                         </div>
                       </dl>
                     </div>
+                  </div>
+
+                  <div id="certifications" class="mt-16 md:-mt-2">
+                    <span className="block text-sm text-gray-400 text-center sm:text-left">Azienda certificata</span>
+                    <img src="soa5.png" alt="Certificazione SOA" className="block w-40 sm:w-52 mt-1 block mx-auto sm:m-0 sm:mt-1" />
+                    <p className="mt-4 text-gray-400 mx-auto sm:ml-0 max-w-xs sm:max-w-sm text-sm text-center sm:text-left">Abbiamo conseguito la certificazione SOA categoria OG3 (classifica II) e categoria OG1 (classifica II).</p>
+                    {
+                      /* <ul className="inline-block list-disc mt-4 text-sm px-4">
+                        <li>Certificazione SOA cat. OG3 classifica II</li>
+                        <li>Certificazione SOA cat. OG1 classifica II</li>
+                      </ul> */
+                    }
                   </div>
 
                 </div>
