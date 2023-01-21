a6493727d03b2ce2af45bb6f41c97c5bbfc66b76
sekmo <francescojjmari@gmail.com>
2023-01-21 15:48:59 +0100

    Remove comments

--- components/Gallery.js
+++ components/Gallery.js
@@ -11,30 +11,6 @@ export default function Gallery(props) {
       pswpModule: () => import('photoswipe'),
     });
 
-    // lightbox.on('loadComplete', ({ content, slide }) => {
-    //   console.log(content);
-    //   // let img = content.element;
-    //   content.height = 200;
-    //   content.width = 200;
-    //   content.data.height = 200;
-    //   content.data.width = 200;
-    //   content.data.h = 200;
-    //   content.data.w = 200;
-    // });
-
-    // lightbox.on('loadComplete', function(index, item) {
-      
-    //   var linkEl = item.el.children[0];
-    //   var img = item.container.children[0];
-    //   if (!linkEl.getAttribute('data-size')) {
-    //     linkEl.setAttribute('data-size', img.naturalWidth + 'x' + img.naturalHeight);
-    //     item.w = img.naturalWidth;
-    //     item.h = img.naturalHeight;
-    //     lightbox.invalidateCurrItems();
-    //     lightbox.updateSize(true);
-    //   }
-    // });
-
     lightbox.init();
 
     return () => {
