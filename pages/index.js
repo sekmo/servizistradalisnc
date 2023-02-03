4f35c2ac5d63876161fcf2427f65410632319867
sekmo <francescojjmari@gmail.com>
2023-02-04 00:06:42 +0100

    Remove example data

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,6 @@ const flickityOptions = {
     autoPlay: 4000
 }
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
 const navigation_items = [
   { name: 'Home', href: '#', current: true },
   { name: 'Azienda', href: '#azienda', current: false },
@@ -25,11 +19,6 @@ const navigation_items = [
   { name: 'Parco Mezzi', href: '#parco-mezzi', current: false },
   { name: 'Realizzazioni', href: '#realizzazioni', current: false },
   { name: 'Contatti', href: '#contatti', current: false },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
 ]
 
 const parco_mezzi_gallery = [
